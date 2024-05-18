42f7ef571e56bd382e8bf70563b64357135338d5
Arda Yasar <50085179+ardayasar@users.noreply.github.com>
2024-05-18 17:51:17 +0300

    Initial Commit

---
+++ kuasarkrod/kuasarkrodApp.swift
@@ -0,0 +1,17 @@
+//
+//  kuasarkrodApp.swift
+//  kuasarkrod
+//
+//  Created by Arda Yasar on 18.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct kuasarkrodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
